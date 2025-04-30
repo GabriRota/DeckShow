ad3c4463525d6e231501ff116c3fe7e1da5e4949
GabriRota <156823326+GabriRota@users.noreply.github.com>
2025-04-30 19:16:50 +0200

    piccoli cambiamenti

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Profile, Post
 
 # Register your models here.
-admin.site.register(Profile)
-admin.site.register(Post)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'id_user', 'name', 'surname', 'nationality')
+    search_fields = ('user__username', 'name', 'surname')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'conditions', 'date_time')
+    search_fields = ('user', 'title', 'id')
+    list_filter = ('conditions', 'date_time')
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Post, PostAdmin)
